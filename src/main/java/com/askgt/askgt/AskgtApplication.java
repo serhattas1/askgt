d8568e285bf05bda62e82c2f402f2b1278678e4b
serhattas1 <serhattas91@hotmail.com>
2025-02-12 16:22:30 +0300

    first commit

---
+++ src/main/java/com/askgt/askgt/AskgtApplication.java
@@ -0,0 +1,13 @@
+package com.askgt.askgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskgtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/askgt/askgt/AskgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.askgt.askgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
